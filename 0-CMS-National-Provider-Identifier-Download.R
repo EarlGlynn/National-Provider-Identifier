4580f559c711ef986ea09ce40032c63e6b2c6b2d
Earl F Glynn <efglynn@gmail.com>
2014-09-25 08:54:18 -0500

    Add script to download National Provider Identifier (NPI) file

---
+++ 0-CMS-National-Provider-Identifier-Download.R
@@ -0,0 +1,71 @@
+# National Plan and Provider Enumeration System (NPPES) Downloadable File
+# http://www.cms.gov/Regulations-and-Guidance/HIPAA-Administrative-Simplification/NationalProvIdentStand/DataDissemination.html
+
+# Download National Provider Identifier (NPI) Downloadable File.
+# http://nppes.viva-it.com/NPI_Files.html
+
+# UMKC Center for Health Insights
+# Earl F Glynn, 2014-09-24.
+
+##############################################################################
+### Setup
+
+#setwd("C:/Data/US-Government/Centers-for-Medicare-and-Medicaid-Services/National-Provider-Identifier/")  ##### Modify as appropriate
+setwd("E:/Government/Federal/HHS/CMS/National-Provider-Identifier/")                                     ##### Modify as appropriate
+
+sink("0-CMS-National-Provider-Identifier-Download.txt", split=TRUE)
+print(Sys.time())
+
+library(downloader)  # platform neutral download function
+library(tools)       # md5sum
+
+DATA.DIR <- "DATA"
+if (! file.exists(DATA.DIR) )
+{
+  dir.create(DATA.DIR)
+}
+
+##############################################################################
+### Download
+
+download.url <- "http://nppes.viva-it.com/NPPES_Data_Dissemination_September_2014.zip"
+zip.filename <- paste0(DATA.DIR, "/NPPES_Data_Dissemination.zip")
+download(download.url, zip.filename, mode = "wb")
+print( md5sum(zip.filename) )
+files <- unzip(zip.filename, exdir=DATA.DIR)
+print(Sys.time())
+
+options(width=150)
+print( file.info(files) )
+
+##############################################################################
+### Preliminary stats.
+### Need about 24 GB memory to read the raw data file.
+
+# For now, treat all fields as character data.
+firstlook <- read.csv(files[1], colClasses="character")
+print(object.size(firstlook))
+str(firstlook)
+
+# Verify NPI might be record key.
+length(unique(firstlook$NPI))
+
+options(width=100)
+for (i in 1:ncol(firstlook))
+{
+  cat("\n*************************************************\n")
+  cat(i, names(firstlook)[i], "\n")
+  counts <- table(firstlook[,i])
+  cat(length(counts), "values\n")
+  if (length(counts) > 100)
+  {
+    cat("Top 100 by frequency ...\n")
+    counts <- sort(counts, decreasing=TRUE)[1:100]
+  }
+  print(counts)
+  flush.console()
+}
+
+print(Sys.time())
+sink()
+
--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-NPI
-===
+National Provider Identifier (NPI) Downloadable File
+====================================================
 
-Working with National Provider Identifier (NPI) Downloadable File
+The Centers for Medicare and Medicaid Services provide a huge file of providers called by either of these names:
+
+* [National Plan and Provider Enumeration System (NPPES) Downloadable File](http://www.cms.gov/Regulations-and-Guidance/HIPAA-Administrative-Simplification/NationalProvIdentStand/DataDissemination.html).
+
+or
+
+* [National Provider Identifier (NPI) Downloadable File](http://nppes.viva-it.com/NPI_Files.html).
+
+The "Full Replacement Monthly NPI File" in Sept. 2014 was a 459 MB ZIP that becomes a huge 4.95 GB file when decompressed.
+
+The complete file in Sept. 2014 had 4,384,814 observations of 329 variables.
+
+The file is extremely bloated -- 2.8 billion of the 5.3 billion characters in the file are the double quotes (") used to surround each field in the CSV file.  The file contains 1.4 billion commas to separate the many empty fields. The several repeating groups of variables should be made into separate database tables.
+
+R scripts
+---------
+
+**0-CMS-National-Provider-Identifier-Download.R**:  Script to download the complete monthly file and take a quick look at frequency counts of values by variable.
